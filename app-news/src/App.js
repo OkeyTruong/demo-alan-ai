a96fc9fff9424ea542cac8bdac6ac5d191dae4c3
Huy Truong <huy.truong@team.enouvo.com>
2022-07-12 12:42:49 +0700

    setup Alan Ai

--- app-news/src/App.js
+++ app-news/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+const alanKey = 'b44cb4a3acf93e0c75605239fd87c3c62e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand:({ command }) => {
+                if (command === 'testCommand') {
+                    alert("sss")
+                }
+            }
+        })
+    },[])
+
   return (
-    <div>App</div>
+      <div>App</div>
   )
 }
 
